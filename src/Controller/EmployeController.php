9eafc1e6067d39a36087cf6df3f5be4e387c50c7
Gautier Cataneo <gautier.cattaneo@stagiaire-elan.fr>
2023-10-06 15:35:50 +0200

    Changes to be committed: 	modified:   .env 	new file:   migrations/Version20231006084717.php 	new file:   public/css/js/script.js 	new file:   public/css/style.css 	new file:   public/js/script.js 	new file:   src/Controller/EmployeController.php 	new file:   src/Controller/EntrepriseController.php 	new file:   src/Entity/Employe.php 	new file:   src/Entity/Entreprise.php 	new file:   src/Form/EmployeType.php 	new file:   src/Form/EntrepriseType.php 	new file:   src/Repository/EmployeRepository.php 	new file:   src/Repository/EntrepriseRepository.php 	modified:   templates/base.html.twig 	new file:   templates/employe/index.html.twig 	new file:   templates/employe/new.html.twig 	new file:   templates/employe/show.html.twig 	new file:   templates/entreprise/index.html.twig 	new file:   templates/entreprise/new.html.twig 	new file:   templates/entreprise/show.html.twig

---
+++ src/Controller/EmployeController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employe;
+use App\Form\EmployeType;
+use App\Repository\EmployeRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EmployeController extends AbstractController
+{
+    #[Route('/employe', name: 'app_employe')]
+    public function index(EmployeRepository $employeRepository): Response
+    {
+        // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        $employes = $employeRepository->findBy([],["nom" => "ASC"]);
+        return $this->render('employe/index.html.twig', [
+            'employes' => $employes,
+        ]);
+    }
+
+    #[Route('/employe/new', name: 'new_employe')]
+    #[Route('/employe/{id}/edit', name: 'edit_employe')]
+    public function new(Employe $employe = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        {
+            if(!$employe){
+                $employe = new Employe();
+            }
+    
+            $form = $this->createForm(EmployeType::class, $employe);
+    
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+              
+                $employe = $form->getData();
+                // prepare PDO
+                $entityManager->persist($employe);
+                // execute PDO
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_employe');
+            }
+
+            return $this->render('employe/new.html.twig', [
+                'formAddEmploye' => $form,
+                'edit' => $employe->getId()
+            ]);
+        }
+    }
+    
+    #[Route('/employe/{id}/delete', name: 'delete_employe')]
+    public function delete(Employe $employe, EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($employe);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_employe');
+    }
+
+    #[Route('/employe/{id}', name: 'show_employe')]
+    public function show(Employe $employe): Response
+    {
+        return $this->render('employe/show.html.twig', [
+            'employe' => $employe
+        ]);
+    }
+}
